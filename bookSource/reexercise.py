9f3765c9bf5d18136db7866b47cb3c4f1a884110
Tim Prescott <teepeemm@gmail.com>
2025-07-30 19:46:35 -0500

    changing $$ to \[ \]

---
+++ bookSource/reexercise.py
@@ -0,0 +1,26 @@
+#!/usr/local/bin/python3
+
+import re
+from glob import glob
+
+for exfilename in glob('*.tex'):
+    with open(exfilename) as exfile:
+        lines = exfile.read()
+    if r'$$' not in lines:
+        continue
+#    print(exfilename)
+#    if not re.search(r'(?<!\\pdftooltip{)\\begin{tikzpicture}(.*?)\\end{tikzpicture}', lines, re.DOTALL):
+#        breakpoint()
+#    quit()
+    lines = re.sub(r'\$\$(.*?)\$\$', r'\\[\1\\]', lines, flags=re.DOTALL)
+#    lines = re.sub(r'\\pdftooltip{\\begin{tikzpicture}\[(.*?)\\end{tikzpicture}}{(.*?)}',
+#                    r'\\begin{tikzpicture}[alt={\2},\1\\end{tikzpicture}',
+#                    lines, flags=re.DOTALL)
+#    lines = re.sub(r'\\pdftooltip{\\begin{tikzpicture}(.*?)\\end{tikzpicture}}{(.*?)}',
+#                    r'\\begin{tikzpicture}[alt={\2}]\1\\end{tikzpicture}',
+#                    lines, flags=re.DOTALL)
+#    lines = re.sub(r'(?<!\\pdftooltip{)\\begin{tikzpicture}(.*?)\\end{tikzpicture}',
+#                    r'\\pdftooltip{\\begin{tikzpicture}\1\\end{tikzpicture}}{ALT-' 'TEXT-TO-BE-DETERMINED}',
+#                    lines, flags=re.DOTALL)
+    with open(exfilename, 'w') as exfile:
+        exfile.write(lines)
